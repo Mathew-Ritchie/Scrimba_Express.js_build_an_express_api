173918fde132eceaf0c588d84a65f5d89b684809
Mathew Ritchie <ritchmat89@gmail.com>
2025-10-16 17:10:06 +0200

    worked with express.Router() and modularised code.

--- server.js
+++ server.js
@@ -1,59 +1,11 @@
 import express from "express";
-import { startups } from "./data/data.js";
+import { apiRouter } from "./routes/apiRouter.js";
 
 const PORT = 8000;
+
 const app = express();
 
-app.get("/api", (req, res) => {
-  let filteredData = startups;
-
-  const { industry, country, continent, is_seeking_funding, has_mvp } = req.query;
-
-  if (industry) {
-    filteredData = filteredData.filter(
-      (startUp) => startUp.industry.toLowerCase() === industry.toLowerCase()
-    );
-  }
-  if (country) {
-    filteredData = filteredData.filter(
-      (startUp) => startUp.country.toLowerCase() === country.toLowerCase()
-    );
-  }
-  if (continent) {
-    filteredData = filteredData.filter(
-      (startUp) => startUp.continent.toLowerCase() === continent.toLowerCase()
-    );
-  }
-  if (is_seeking_funding) {
-    filteredData = filteredData.filter(
-      (startUp) => startUp.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
-    );
-  }
-  if (has_mvp) {
-    filteredData = filteredData.filter(
-      (startUp) => startUp.has_mvp === JSON.parse(has_mvp.toLowerCase())
-    );
-  }
-
-  res.json(filteredData);
-});
-
-app.get("/api/:field/:term", (req, res) => {
-  const { field, term } = req.params;
-  const allowedFields = ["industry", "country", "continent"];
-
-  if (!allowedFields.includes(field)) {
-    return res.status(400).json({
-      message: "Search field not allowed. Please use only 'country', 'continent', 'industry'",
-    });
-  }
-
-  const filteredData = startups.filter(
-    (startup) => startup[field].toLowerCase() === term.toLowerCase()
-  );
-
-  res.json(filteredData);
-});
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
