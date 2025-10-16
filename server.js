0cd2bd7a112815459d0955a967308b266b9e3c33
Mathew Ritchie <ritchmat89@gmail.com>
2025-10-16 10:08:39 +0200

    setting up query params.

--- server.js
+++ server.js
@@ -5,7 +5,37 @@ const PORT = 8000;
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json(startups);
+  let filteredData = startups;
+
+  const { industry, country, continent, is_seeking_funding, has_mvp } = req.query;
+
+  if (industry) {
+    filteredData = filteredData.filter(
+      (startUp) => startUp.industry.toLowerCase() === industry.toLowerCase()
+    );
+  }
+  if (country) {
+    filteredData = filteredData.filter(
+      (startUp) => startUp.country.toLowerCase() === country.toLowerCase()
+    );
+  }
+  if (continent) {
+    filteredData = filteredData.filter(
+      (startUp) => startUp.continent.toLowerCase() === continent.toLowerCase()
+    );
+  }
+  if (is_seeking_funding) {
+    filteredData = filteredData.filter(
+      (startUp) => startUp.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
+    );
+  }
+  if (has_mvp) {
+    filteredData = filteredData.filter(
+      (startUp) => startUp.has_mvp === JSON.parse(has_mvp.toLowerCase())
+    );
+  }
+
+  res.json(filteredData);
 });
 
 app.listen(PORT, () => {
