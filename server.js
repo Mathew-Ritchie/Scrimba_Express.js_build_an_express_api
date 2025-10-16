4c1fdce51ff1163cdff8acc8a3a123a0ef19a8f2
Mathew Ritchie <ritchmat89@gmail.com>
2025-10-16 17:18:12 +0200

    added cors for cross domain access

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const PORT = 8000;
 
 const app = express();
 
+app.use(cors());
+
 app.use("/api", apiRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Endpoint not found. Please check the API documentation." });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
